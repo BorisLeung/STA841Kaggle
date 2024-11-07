aa91f9233fccfe9144202dd9e6373980db45491a
Boris Leung <borisleung2013@gmail.com>
2024-11-06 21:35:58 -0500

    Merge data related files from logistic branch

---
+++ data.py
@@ -0,0 +1,47 @@
+import os
+
+import numpy as np
+import pandas as pd
+
+DATA_DIR = "data"
+PROCESSED_DIR = "processed"
+
+edu_train_data = "module_Education_train_set.csv"
+edu_test_data = "module_Education_test_set.csv"
+house_train_data = "module_HouseholdInfo_train_set.csv"
+house_test_data = "module_HouseholdInfo_test_set.csv"
+pov_train_data = "module_SubjectivePoverty_train_set.csv"
+sample_submission_data = "sample_submission.csv"
+
+edu_train = pd.read_csv(os.path.join(DATA_DIR, edu_train_data))
+edu_test = pd.read_csv(os.path.join(DATA_DIR, edu_test_data))
+house_train = pd.read_csv(os.path.join(DATA_DIR, house_train_data))
+house_test = pd.read_csv(os.path.join(DATA_DIR, house_test_data))
+pov_train = pd.read_csv(os.path.join(DATA_DIR, pov_train_data))
+sample_submission = pd.read_csv(os.path.join(DATA_DIR, sample_submission_data))
+combined_train = pd.read_csv(os.path.join(PROCESSED_DIR, "combined_train.csv"))
+combined_train_with_num_pov = combined_train.copy()
+combined_train_with_num_pov["num_pov"] = (
+    np.argmax(
+        combined_train_with_num_pov.filter(like="subjective_poverty").values, axis=1
+    )
+    + 1
+)
+combined_test = pd.read_csv(os.path.join(PROCESSED_DIR, "combined_test.csv"))
+
+HOUSE_PREFIX = "house_"
+EDU_PREFIX = "edu_"
+
+__all__ = [
+    "edu_train",
+    "edu_test",
+    "house_train",
+    "house_test",
+    "pov_train",
+    "sample_submission",
+    "HOUSE_PREFIX",
+    "EDU_PREFIX",
+    "combined_train",
+    "combined_train_with_num_pov",
+    "combined_test",
+]
